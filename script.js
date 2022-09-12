0f8a44325bbb82f06cd26237bf3f3b8a0f808fd8
Atanu Malik <111167455+AtanuMalik@users.noreply.github.com>
2022-09-12 11:02:29 +0530

    Change in image location

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ button.onclick = function() {
     let bulb = document.getElementById("bulb");
     
     if (button.innerHTML == "ON") {
-        bulb.setAttribute("src", "/Bulb/bulb-on.jpg")
+        bulb.setAttribute("src", "/bulb-on.jpg")
         button.innerHTML = "OFF";
     } else if(button.innerHTML == "OFF"){
-        bulb.setAttribute("src", "/Bulb/bulb-off.jpg")
+        bulb.setAttribute("src", "/bulb-off.jpg")
         button.innerHTML = "ON";
     } else {
-        bulb.setAttribute("src", "/Bulb/bulb-on.jpg")
+        bulb.setAttribute("src", "/bulb-on.jpg")
         button.innerHTML = "OFF";
     }
 
